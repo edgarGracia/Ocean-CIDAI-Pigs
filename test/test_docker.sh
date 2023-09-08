38f78037bc373059912136cf6edb9569da64b495
Edgar <edgarGrac@gmail.com>
2023-09-08 17:55:31 +0200

    Unnecessary file

--- test/test_docker.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm \
--v "$(pwd)/data/inputs":/data/inputs \
--v "$(pwd)/data/outputs":/data/outputs \
--v "$(pwd)/../entry_point.sh":/data/transformations/algorithm \
--e DIDS='["1234"]' \
-egracia/cidai_pigs:pigs_0_all_det2 bash /data/transformations/algorithm
