879ef96816c2d5aa25a31d572d98f8e4d63769ba
Edgar <edgargrac@gmail.com>
2023-11-28 12:25:48 +0100

    Entry point fix

--- entry_point.sh
+++ entry_point.sh
@@ -1,12 +1,10 @@
-sudo setfacl -m u:$USER:rwx /data
-echo "Processing input path"
 jq -r '.[]' <(echo $DIDS) | while read did; do
-  unzip -o "/data/inputs/$did/0" -d /workdir/input;
-  python /workdir/src/predict_path.py -c /workdir/model/config.yaml -w /workdir/model/model.pth -i /workdir/input -o "/workdir/output/$did" --use-cuda;
-  python /workdir/src/NTracker/main.py --config-name coco_pigs images_path="/workdir/input" annotations_path="/workdir/output/$did/" hydra.run.dir="/workdir/output/$did/tracking/"
+  unzip -o "/data/inputs/$did/0" -d /workdir/input &>> logs;
+  python /workdir/src/predict_path.py -c /workdir/model/config.yaml -w /workdir/model/model.pth -i /workdir/input -o "/workdir/output/$did" --use-cuda &>> logs;
+  python /workdir/src/NTracker/main.py --config-name coco_pigs images_path="/workdir/input" annotations_path="/workdir/output/$did/" hydra.run.dir="/workdir/output/$did/tracking/" &>> logs;
   rm -r /workdir/input;
 done
 
-echo "Exporting output"
-zip -r output.zip /workdir/output/
-mv output.zip /data/outputs/
+zip -r output.zip /workdir/output/ &>> logs
+sudo mv output.zip /data/outputs/
+sudo mv logs /data/outputs/out_logs
